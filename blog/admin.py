328123491d05c520b9b1d8254d881a2611a8d6e2
devinaacs <dde.vina@yahoo.co.id>
2024-07-06 23:58:41 +0700

    create postadmin

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import Post, Author, Tag
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    # readonly_fields = ('slug',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
 admin.site.register(Post)
 admin.site.register(Author)
 admin.site.register(Tag)
