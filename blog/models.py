a82381c030b6dedf4ff896b15e742999651debd9
devinaacs <dde.vina@yahoo.co.id>
2024-07-06 23:53:48 +0700

    create string representation for each model

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,16 @@ from django.core.validators import MinLengthValidator
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
     
+    def __str__(self):
+        return self.caption
+    
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
     
 class Post(models.Model):
     title = models.CharField(max_length=150)
@@ -21,5 +27,7 @@ class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts" )
     tags = models.ManyToManyField(Tag)
     
+    def __str__(self):
+        return self.title
 
     
